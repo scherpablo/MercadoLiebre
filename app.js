d81671f8ede238fbbe8f83dadc6e4a7e97b970cb
scherpablo <scherpablo@gmail.com>
2022-03-24 02:14:02 -0300

    subuiendo mercado liebre

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Servidor corriendo en el puerto 3000');
     });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 
